a164bcb64ae70aad02957ee490d2f6386ac087a1
david.muolo <davidmuolo1@gmail.com>
2021-03-23 13:11:15 -0500

    Metadata, BundleConfig, Template Convert

--- FSDP.DATA.EF/Metadata/FSDPMetadata.cs
+++ FSDP.DATA.EF/Metadata/FSDPMetadata.cs
@@ -11,9 +11,20 @@ namespace FSDP.DATA.EF.Metadata
 
     public class ApplicationMetadata
     {
+        [Required(ErrorMessage = "* Day/time applied is required *")]
+        [Display(Name = "Applied")]
+        [DisplayFormat(DataFormatString = "{0:f}")]
         public System.DateTime ApplicationDate { get; set; }
+
+        [Display(Name = "Notes")]
+        [StringLength(2000, ErrorMessage = "* Notes cannot exceed 2000 characters *")]
+        [UIHint("MultilineText")]
+        [DisplayFormat(NullDisplayText = "None")]
         public string ManagerNotes { get; set; }
-        public int ApplicationStatus { get; set; }
+
+        [Required(ErrorMessage = "* Resume is required for an application *")]
+        [Display(Name = "Resume")]
+        [StringLength(75, ErrorMessage = "* Resume cannot exceed 75 characters *")]
         public string ResumeFilename { get; set; }
     }
 
@@ -29,7 +40,13 @@ namespace FSDP.DATA.EF.Metadata
 
     public class ApplicationStatuMetadata
     {
+        [Required(ErrorMessage = "* Status is required *")]
+        [Display(Name = "Status")]
+        [StringLength(50, ErrorMessage = "* Status cannot exceed 50 characters *")]
         public string StatusName { get; set; }
+
+        [Display(Name = "Description")]
+        [StringLength(250, ErrorMessage = "* Status cannot exceed 250 characters *")]
         public string StatusDescription { get; set; }
     }
 
@@ -45,8 +62,15 @@ namespace FSDP.DATA.EF.Metadata
 
     public class LocationMetadata
     {
+        [Required(ErrorMessage = "* City is required *")]
+        [StringLength(50, ErrorMessage = "* City cannot exceed 50 characters *")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "* State is required *")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = " State cannot exceed 50 characters *")]
         public string State { get; set; }
+
+        [Display(Name = "Manager ID")]
         public string ManagerId { get; set; }
     }
 
@@ -62,7 +86,11 @@ namespace FSDP.DATA.EF.Metadata
 
     public class OpenPositionMetadata
     {
+        [Display(Name = "Position ID")]
+        public int PositionId { get; set; }
 
+        [Display(Name = "Location ID")]
+        public int LocationId { get; set; }
     }
 
     [MetadataType(typeof(OpenPositionMetadata))]
@@ -77,7 +105,11 @@ namespace FSDP.DATA.EF.Metadata
 
     public class PositionMetadata
     {
+        [Required(ErrorMessage = "* Position title is required *")]
+        [StringLength(50, ErrorMessage = "* Position title cannot exceed 50 characters *")]
         public string Title { get; set; }
+
+        [UIHint("MultilineText")]
         public string JobDescription { get; set; }
     }
 
@@ -93,12 +125,33 @@ namespace FSDP.DATA.EF.Metadata
 
     public class UserDetailMetadata
     {
+        [Required(ErrorMessage = "*First Name is required *")]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "*First Name cannot exceed 50 characters *")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*Last Name is required *")]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "*Last Name cannot exceed 50 characters *")]
         public string LastName { get; set; }
+
+        //Validation done manually inside the controller
         public string Image { get; set; }
+
+        [Display(Name = "Instrument 1")]
+        [StringLength(50, ErrorMessage = "* Instrument 1 cannot exceed 50 characters *")]
         public string Instrument1 { get; set; }
+
+        [Display(Name = "Instrument 2")]
+        [StringLength(50, ErrorMessage = "* Instrument 2 cannot exceed 50 characters *")]
         public string Instrument2 { get; set; }
+
+        [UIHint("MultilineText")]
+        [StringLength(500, ErrorMessage = "* Related skills cannot exceed 500 characters *")]
         public string RelatedSkills { get; set; }
+
+        [Display(Name = "Upload Resume")]
+        [StringLength(75, ErrorMessage = "* Resume file name cannot exceed 75 characters *")]
         public string ResumeFilename { get; set; }
     }
 
--- FSDP.UI.MVC/App_Start/BundleConfig.cs
+++ FSDP.UI.MVC/App_Start/BundleConfig.cs
@@ -7,24 +7,44 @@ namespace FSDP.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/vendor/owl.carousel2/assets/owl.carousel.min.css",
+                "~/Content/vendor/owl.carousel2/assets/owl.theme.default.min.css",
+                "~/Content/vendor/bootstrap-select/css/bootstrap-select.min.css",
+                "~/Content/vendor/lightbox2/css/lightbox.min.css",
+                "~/Content/css/style.default.css",
+                "~/Content/css/custom.css"
+                ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/vendor/owl.carousel2/owl.carousel.min.js",
+                "~/Content/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js",
+                "~/Content/vendor/bootstrap-select/js/bootstrap-select.min.js",
+                "~/Content/vendor/lightbox2/js/lightbox.min.js",
+                "~/Content/js/bootstrap-filestyle.min.js",
+                "~/Content/js/front.js"
+                ));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
         }
     }
 }
